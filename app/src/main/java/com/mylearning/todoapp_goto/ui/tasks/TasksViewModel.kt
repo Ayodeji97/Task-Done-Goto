19f7a0a97e86ca5bb1308a82046f42270d5ef965
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-12-04 12:46:44 +0100

    added data to recyclerview

--- app/src/main/java/com/mylearning/todoapp_goto/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/mylearning/todoapp_goto/ui/tasks/TasksViewModel.kt
@@ -2,8 +2,12 @@ package com.mylearning.todoapp_goto.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.mylearning.todoapp_goto.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor (private val taskDao: TaskDao): ViewModel() {
+
+    val task = taskDao.getTasks().asLiveData()
+
 
 }
