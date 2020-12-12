255b643c2d71a05beb3203274610bddb4e8fbb16
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-12-12 10:11:49 +0100

    combine multiple flow to be emitted on the ui

--- app/src/main/java/com/mylearning/todoapp_goto/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/mylearning/todoapp_goto/ui/tasks/TasksViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.mylearning.todoapp_goto.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
 class TasksViewModel @ViewModelInject constructor (private val taskDao: TaskDao): ViewModel() {
@@ -14,11 +15,14 @@ class TasksViewModel @ViewModelInject constructor (private val taskDao: TaskDao)
     val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
     val hideCompleted = MutableStateFlow(false)
 
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    private val tasksFlow = combine(searchQuery, sortOrder, hideCompleted ) { query, sortOrder, hideComplete ->
+        // lamda func : whenever any of the flow emit a new value, this will be called
+        Triple(query, sortOrder, hideComplete)
+    }.flatMapLatest { (query, sortOrder, hideComplete) ->
+        taskDao.getTasks(query, sortOrder, hideComplete)
     }
     val task = tasksFlow.asLiveData()
 
 }
 
-enum class SortOrder {BY_Name, BY_DATE }
+enum class SortOrder {BY_NAME, BY_DATE }
